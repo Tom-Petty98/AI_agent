00a8159c560c47b431e64fd3ba5eb2fa4d521572
Tom-Petty98 <tompetty527@yahoo.co.uk>
2025-07-18 11:17:22 +0100

    read and write file functions

---
+++ functions/write_file.py
@@ -0,0 +1,18 @@
+import os
+
+def write_file(working_directory, file_path, content):
+    abs_working_dir = os.path.abspath(working_directory)
+    abs_file_path = os.path.abspath(os.path.join(working_directory, file_path))
+
+    if not abs_file_path.startswith(abs_working_dir):
+        return f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
+    if not os.path.isfile(abs_file_path):
+        return f'Error: File not found or is not a regular file: "{file_path}"'
+    
+    try:
+        with open(abs_file_path, "w") as f:
+            f.write(content)
+        return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
+    
+    except Exception as e:
+        return f'Error reading file "{file_path}": {e}'
--- tests.py
+++ tests.py
@@ -25,24 +25,46 @@ from functions.get_files_info import get_files_info
 # if __name__ == "__main__":
 #     unittest.main()
 
+# def test():
+#     result = get_files_info("calculator", ".")
+#     print("Result for current directory:")
+#     print(result)
+#     print("")
+
+#     result = get_files_info("calculator", "pkg")
+#     print("Result for 'pkg' directory:")
+#     print(result)
+
+#     result = get_files_info("calculator", "/bin")
+#     print("Result for '/bin' directory:")
+#     print(result)
+
+#     result = get_files_info("calculator", "../")
+#     print("Result for '../' directory:")
+#     print(result)
+
+# if __name__ == "__main__":
+#     test()
+
+# from functions.get_file_info import get_file_content
+
+# def test():
+#     # print(get_file_content("calculator", "lorem.txt"))
+#     print(get_file_content("calculator", "main.py"))
+#     print(get_file_content("calculator", "pkg/calculator.py"))
+#     print(get_file_content("calculator", "/bin/cat"))
+#     print(get_file_content("calculator", "pkg/does_not_exist.py"))
+
+# if __name__ == "__main__":
+#     test()
+
+
+from functions.write_file import write_file
+
 def test():
-    result = get_files_info("calculator", ".")
-    print("Result for current directory:")
-    print(result)
-    print("")
-
-    result = get_files_info("calculator", "pkg")
-    print("Result for 'pkg' directory:")
-    print(result)
-
-    result = get_files_info("calculator", "/bin")
-    print("Result for '/bin' directory:")
-    print(result)
-
-    result = get_files_info("calculator", "../")
-    print("Result for '../' directory:")
-    print(result)
-
+    print(write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum"))
+    print(write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet"))
+    print(write_file("calculator", "/tmp/temp.txt", "this should not be allowed"))
 
 if __name__ == "__main__":
     test()
